d64a0ce90fd105c36fb6e64cd6f4cfeb56014b08
Zielin0 <me@zielinus.xyz>
2023-01-01 20:51:39 +0100

    feat(*): Hello mech 🚀

---
+++ mech.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/gookit/color"
+	"github.com/shirou/gopsutil/v3/host"
+	"github.com/shirou/gopsutil/v3/mem"
+)
+
+func printHeader() {
+	host, _ := host.Info()
+	username := os.Getenv("USERNAME")
+
+	color.FgLightRed.Print(username)
+	color.FgWhite.Print("@")
+	color.FgLightRed.Println(host.Hostname)
+}
+
+func getOS() string {
+	host, _ := host.Info()
+	return host.Platform
+}
+
+func getKernel() string {
+	host, _ := host.Info()
+	if host.OS == "windows" {
+		return strings.Split(host.KernelVersion, " ")[0]
+	}
+	return host.KernelVersion
+}
+
+func getUptime() string {
+	host, _ := host.Info()
+	days := host.Uptime / 60 / 60 / 24
+	hours := host.Uptime / 60 / 60 % 24
+	minutes := host.Uptime / 60 % 60
+	seconds := host.Uptime % 60
+	uptime := ""
+	if days != 0 {
+		uptime = fmt.Sprintf("%s%dd ", uptime, days)
+	}
+	if hours != 0 {
+		uptime = fmt.Sprintf("%s%dh ", uptime, hours)
+	}
+	if minutes != 0 {
+		uptime = fmt.Sprintf("%s%dm ", uptime, minutes)
+	}
+	if seconds != 0 {
+		uptime = fmt.Sprintf("%s%ds ", uptime, seconds)
+	}
+	return uptime
+}
+
+func getMemory() string {
+	mem, _ := mem.VirtualMemory()
+	used := mem.Used / 1024 / 1024 / 1000
+	free := mem.Free / 1024 / 1024 / 1000
+	full := used + free
+	return fmt.Sprintf("%dGiB / %dGiB", used, full)
+}
+
+func main() {
+	printHeader()
+	fmt.Println(getOS())
+	fmt.Println(getKernel())
+	fmt.Println(getUptime())
+	fmt.Println(getMemory())
+}
